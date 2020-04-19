897c5d1e267aa1a2f46b6be5e8c9f7be72bbd077
sahunsh <63538365+sahunsh@users.noreply.github.com>
2020-04-19 10:13:18 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,2 @@
-apt-get update
-apt-get install apache2 -y
-apt-get install docker.io -y
+docker pull sahunsh/phpmyadmin_mysql2
+docker run -it -v /home/ubuntu/jenkins/workspace/project/php:/var/www/html -p 80:80 -p 3306:3306 -d sahunsh/phpmyadmin_mysql2
